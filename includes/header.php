65e9695c677216b5d1ce817f5f31218ce803a49a
salerno00 <gsalernoambrosio00@gmail.com>
2026-05-16 19:06:20 -0300

    adicionar cabeçalho do site

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Farmácia United</title>
+</head>
+
+<body>
+
+<header>
+    <h1> Farmácia United</h1>
+
+    <nav class="hotbar">
+        <a href="index.php">Produtos</a>
+        <a href="create.php">Cadastrar</a>
+    </nav>
+</header>
+
+<main>
